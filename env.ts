5f6249ff5f8df0e8d088a43dd99647f6b5ce0058
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2025-02-03 18:54:11 +0400

    Update environment configuration to use AWS credentials

--- env.ts
+++ env.ts
@@ -3,9 +3,11 @@ import zod from 'zod';
 const envSchema = zod.object({
   AUTH_SECRET: zod.string().min(1).default('auth-secret'),
   AUTH_TRUST_HOST: zod.string().min(1).default('true'),
-  DATABASE_URL: zod.string().min(1),
   AUTH_GOOGLE_ID: zod.string().min(1),
-  AUTH_GOOGLE_SECRET: zod.string().min(1)
+  AUTH_GOOGLE_SECRET: zod.string().min(1),
+  AWS_ACCESS_KEY_ID: zod.string().min(1),
+  AWS_SECRET_ACCESS_KEY: zod.string().min(1),
+  AWS_REGION: zod.string().min(1).default('eu-west-1')
 });
 
 export const env = envSchema.parse(process.env);
