e40c22fff0d3c426a0ed9abac6e34bc592585861
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2025-02-04 10:40:04 +0400

    Prepare DynamoDB authentication adapter configuration (commented out)

--- src/lib/auth.config.ts
+++ src/lib/auth.config.ts
@@ -1,6 +1,8 @@
 import { env } from '@/env';
+// import { DynamoDBAdapter } from '@auth/dynamodb-adapter';
 import { NextAuthConfig } from 'next-auth';
 import GoogleProvider from 'next-auth/providers/google';
+// import { ddbDocClient } from './dynamodb';
 
 const authConfig = {
   providers: [
@@ -10,9 +12,28 @@ const authConfig = {
       allowDangerousEmailAccountLinking: true
     })
   ],
+  // adapter: DynamoDBAdapter(ddbDocClient, {
+  //   tableName: 'chat-demo',
+  //   partitionKey: 'pk',
+  //   sortKey: 'sk',
+  //   indexName: 'email-index',
+  //   indexPartitionKey: 'email'
+  // }),
   pages: {
     signIn: '/'
   }
+  // session: {
+  //   strategy: 'database'
+  // },
+  // callbacks: {
+  //   session: ({ session, user }) => ({
+  //     ...session,
+  //     user: {
+  //       ...session.user,
+  //       id: user.id
+  //     }
+  //   })
+  // }
 } satisfies NextAuthConfig;
 
 export default authConfig;
