28411e552593a7cc6dadf7f4c4fce9d67064bc22
Roman Letodiani💠 <roman.letodiani03@gmail.com>
2025-02-05 11:38:54 +0400

    Update pie graph to display sentiment analysis metrics

--- src/features/overview/components/pie-graph.tsx
+++ src/features/overview/components/pie-graph.tsx
@@ -18,50 +18,52 @@ import {
   ChartTooltip,
   ChartTooltipContent
 } from '@/components/ui/chart';
+
 const chartData = [
-  { browser: 'chrome', visitors: 275, fill: 'var(--color-chrome)' },
-  { browser: 'safari', visitors: 200, fill: 'var(--color-safari)' },
-  { browser: 'firefox', visitors: 287, fill: 'var(--color-firefox)' },
-  { browser: 'edge', visitors: 173, fill: 'var(--color-edge)' },
-  { browser: 'other', visitors: 190, fill: 'var(--color-other)' }
+  { sentiment: 'positive', count: 1, fill: 'var(--color-positive)' },
+  { sentiment: 'neutral', count: 1, fill: 'var(--color-neutral)' },
+  { sentiment: 'negative', count: 1, fill: 'var(--color-negative)' }
+  // { browser: 'edge', visitors: 173, fill: 'var(--color-edge)' },
+  // { browser: 'other', visitors: 190, fill: 'var(--color-other)' }
 ];
 
 const chartConfig = {
-  visitors: {
-    label: 'Visitors'
-  },
-  chrome: {
-    label: 'Chrome',
+  positive: {
+    label: 'Positive',
     color: 'hsl(var(--chart-1))'
   },
-  safari: {
-    label: 'Safari',
+  neutral: {
+    label: 'Neutral',
     color: 'hsl(var(--chart-2))'
   },
-  firefox: {
-    label: 'Firefox',
+  negative: {
+    label: 'Negative',
     color: 'hsl(var(--chart-3))'
-  },
-  edge: {
-    label: 'Edge',
-    color: 'hsl(var(--chart-4))'
-  },
-  other: {
-    label: 'Other',
-    color: 'hsl(var(--chart-5))'
   }
+  // firefox: {
+  //   label: 'Firefox',
+  //   color: 'hsl(var(--chart-3))'
+  // },
+  // edge: {
+  //   label: 'Edge',
+  //   color: 'hsl(var(--chart-4))'
+  // },
+  // other: {
+  //   label: 'Other',
+  //   color: 'hsl(var(--chart-5))'
+  // }
 } satisfies ChartConfig;
 
 export function PieGraph() {
   const totalVisitors = React.useMemo(() => {
-    return chartData.reduce((acc, curr) => acc + curr.visitors, 0);
+    return chartData.reduce((acc, curr) => acc + curr.count, 0);
   }, []);
 
   return (
     <Card className='flex flex-col'>
       <CardHeader className='items-center pb-0'>
-        <CardTitle>Pie Chart - Donut with Text</CardTitle>
-        <CardDescription>January - June 2024</CardDescription>
+        <CardTitle>Sentiment Analysis</CardTitle>
+        <CardDescription>January - February 2025</CardDescription>
       </CardHeader>
       <CardContent className='flex-1 pb-0'>
         <ChartContainer
@@ -75,8 +77,8 @@ export function PieGraph() {
             />
             <Pie
               data={chartData}
-              dataKey='visitors'
-              nameKey='browser'
+              dataKey='count'
+              nameKey='sentiment'
               innerRadius={60}
               strokeWidth={5}
             >
@@ -102,7 +104,7 @@ export function PieGraph() {
                           y={(viewBox.cy || 0) + 24}
                           className='fill-muted-foreground'
                         >
-                          Visitors
+                          Chats
                         </tspan>
                       </text>
                     );
@@ -115,10 +117,10 @@ export function PieGraph() {
       </CardContent>
       <CardFooter className='flex-col gap-2 text-sm'>
         <div className='flex items-center gap-2 font-medium leading-none'>
-          Trending up by 5.2% this month <TrendingUp className='h-4 w-4' />
+          Trending up by 0% this month <TrendingUp className='h-4 w-4' />
         </div>
         <div className='leading-none text-muted-foreground'>
-          Showing total visitors for the last 6 months
+          Showing total Chats for the last 6 months
         </div>
       </CardFooter>
     </Card>
